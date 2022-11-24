8a14ad92512c473736ec5c6e07a1f711b159b2a4
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-24 07:48:56 +0300

    Completed assignment

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+
+function App() {
+  useEffect(()=>{
+    fetch("/movies")
+    .then(resp => resp.json())
+    .then(movies => console.log(movies))
+
+  }, [])
+
+
+  return <h1>Hello from React!</h1>
+}
+
+export default App;
